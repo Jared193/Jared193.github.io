854adc3494a576516992e4e5873930e54b576538
Travis <tjxhahn@gmail.com>
2018-02-07 21:41:43 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <title>Auldra</title>
+  </head>
+  <body>
+  <?php
+  
+  ?>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-9">
+      
+      </div>
+      <div class="col-xs-3">
+      
+      </div>
+    </div>
+  </div>
+  </body>
+</html>
